16692d229a54e1e55f0c25de457459606cb8c597
Sylphiann <hydrolilium@gmail.com>
2024-12-22 21:32:48 +0700

    Add template and static files

--- Search/views.py
+++ Search/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'data': 'This is a placeholder for index page'
+    }
+    return render(request, 'index.html', context=context)
+
+
+def result(request):
+    context = {
+        'data': 'This is a placeholder for result page'
+    }
+    return render(request, 'result.html', context=context)
+
+
+def detail(request):
+    context = {
+        'data': 'This is a placeholder for detail page'
+    }
+    return render(request, 'detail.html', context=context)
--- SimplyQuery/settings.py
+++ SimplyQuery/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Search',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'SimplyQuery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
